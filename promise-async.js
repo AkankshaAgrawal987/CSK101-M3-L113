990e23e772fb3786141356491461af8d527a6527
AkankshaAgrawal987 <115335697+AkankshaAgrawal987@users.noreply.github.com>
2022-12-13 11:10:38 +0530

    Initial commit

---
+++ promise-async.js
@@ -0,0 +1,39 @@
+const cookies = [{ name: 'Chocolate Cookies' }, { name: 'Macaron Cookies' }];
+const newCookie = { name: 'Biscotti Cookies' };
+
+// Progression 1: create a function to get all the cookies
+
+// Progression 2: using setTimeout() - use 1000 units for time parameter
+function getCookies() {
+  setTimeout(() => {
+    let output = '';
+    for (let i = 0; i < cookies.length; i++) {
+      output += `<li>${cookies[i].name}</li>`;
+    }
+    document.getElementById('list').innerHTML = output;
+  }, 1000);
+}
+
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+function creatCookies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (newCookie.name != '') {
+        resolve(cookies.push(newCookie));
+      } else {
+        console.log('Empty');
+      }
+    }, 2000);
+  });
+}
+// Progression 4: adding two parameters and using setTimeout() - use 2000 units for time parameter
+
+// Progression 5: handling errors and adding new cookie to the list
+
+// Progression 7: creating a new async function
+async function allCookies() {
+  await creatCookies(newCookie);
+  getCookies();
+}
+// calling the new async function
+allCookies();
